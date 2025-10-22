110b041251cb93a42f780a60d97fd62b4dd1922f
Investorold <tglove217@gmail.com>
2025-10-22 15:41:09 +0100

    Clean up deployment: remove TodoList contract and update deploy script

--- contracts/deploy/deploy.ts
+++ contracts/deploy/deploy.ts
@@ -11,14 +11,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   console.log(`TaskManager contract: `, deployedTaskManager.address);
-
-  const deployedTodoList = await deploy("TodoList", {
-    from: deployer,
-    log: true,
-  });
-
-  console.log(`TodoList contract: `, deployedTodoList.address);
 };
+
 export default func;
-func.id = "deploy_taskManager_and_todoList"; // id required to prevent reexecution
-func.tags = ["TaskManager", "TodoList"];
+func.id = "deploy_taskManager"; // id required to prevent reexecution
+func.tags = ["TaskManager"];
